6594610d5544c129a89c26a919af53f4fb82f5e1
LeleSD68 <danielestriuli@gmail.com>
2026-01-29 11:55:50 +0100

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,9 +7,7 @@ import RatingStars from './RatingStars';
 // Icons
 const HomeIcon = () => (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M9.293 2.293a1 1 0 0 1 1.414 0l7 7A1 1 0 0 1 17 11h-1v6a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-6H3a1 1 0 0 1-.707-1.707l7-7Z" clipRule="evenodd" /></svg>);
 const GridIcon = () => (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M4.25 2A2.25 2.25 0 0 0 2 4.25v2.5A2.25 2.25 0 0 0 4.25 9h2.5A2.25 2.25 0 0 0 9 6.75v-2.5A2.25 2.25 0 0 0 6.75 2h-2.5Zm0 9A2.25 2.25 0 0 0 2 13.25v2.5A2.25 2.25 0 0 0 4.25 18h2.5A2.25 2.25 0 0 0 9 15.75v-2.5A2.25 2.25 0 0 0 6.75 11h-2.5Zm9-9A2.25 2.25 0 0 0 11 4.25v2.5A2.25 2.25 0 0 0 13.25 9h2.5A2.25 2.25 0 0 0 18 6.75v-2.5A2.25 2.25 0 0 0 15.75 2h-2.5Zm0 9A2.25 2.25 0 0 0 11 13.25v2.5A2.25 2.25 0 0 0 13.25 18h2.5A2.25 2.25 0 0 0 18 15.75v-2.5A2.25 2.25 0 0 0 15.75 11h-2.5Z" clipRule="evenodd" /></svg>);
-const DiaryIcon = () => (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path d="M5.75 2a.75.75 0 0 1 .75.75V4h7V2.75a.75.75 0 0 1 1.5 0V4h.25A2.75 2.75 0 0 1 18 6.75v8.5A2.75 2.75 0 0 1 15.25 18H4.75A2.75 2.75 0 0 1 2 15.25v-8.5A2.75 2.75 0 0 1 4.75 4H5V2.75A.75.75 0 0 1 5.75 2Z" /><path d="M4.75 5.5a1.25 1.25 0 0 0-1.25 1.25v8.5c0 .69.56 1.25 1.25 1.25h10.5c.69 0 1.25-.56 1.25-1.25v-8.5c0-.69-.56-1.25-1.25-1.25H4.75Z" />
-    </svg>
-);
+const DiaryIcon = () => (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path d="M5.75 2a.75.75 0 0 1 .75.75V4h7V2.75a.75.75 0 0 1 1.5 0V4h.25A2.75 2.75 0 0 1 18 6.75v8.5A2.75 2.75 0 0 1 15.25 18H4.75A2.75 2.75 0 0 1 2 15.25v-8.5A2.75 2.75 0 0 1 4.75 4H5V2.75A.75.75 0 0 1 5.75 2Z" /><path d="M4.75 5.5a1.25 1.25 0 0 0-1.25 1.25v8.5c0 .69.56 1.25 1.25 1.25h10.5c.69 0 1.25-.56 1.25-1.25v-8.5c0-.69-.56-1.25-1.25-1.25H4.75Z" /></svg>);
 const ChartIcon = () => (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path d="M15.5 2A1.5 1.5 0 0 0 14 3.5v8a1.5 1.5 0 0 0 1.5 1.5h1a1.5 1.5 0 0 0 1.5-1.5v-8A1.5 1.5 0 0 0 16.5 2h-1ZM9.5 6A1.5 1.5 0 0 0 8 7.5v4a1.5 1.5 0 0 0 1.5 1.5h1a1.5 1.5 0 0 0 1.5-1.5v-4A1.5 1.5 0 0 0 10.5 6h-1ZM3.5 10A1.5 1.5 0 0 0 2 11.5v0A1.5 1.5 0 0 0 3.5 13h1a1.5 1.5 0 0 0 1.5-1.5v0A1.5 1.5 0 0 0 4.5 10h-1Z" /></svg>);
 const UserGroupIcon = () => (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path d="M7 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM14.5 9a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5ZM1.615 16.428a1.224 1.224 0 0 1-.569-1.175 6.002 6.002 0 0 1 11.908 0c.058.467-.172.92-.57 1.174A9.953 9.953 0 0 1 7 18a9.953 9.953 0 0 1-5.385-1.572ZM14.5 16h-.106c.07-.38.106-.772.106-1.175 0-.537-.067-1.054-.191-1.543A7.001 7.001 0 0 1 17 18a9.952 9.952 0 0 1-2.5-2Z" /></svg>);
 const UploadIcon = () => (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M3 17a1 1 0 0 1 1-1h12a1 1 0 1 1 0 2H4a1 1 0 0 1-1-1Zm3.293-7.707a1 1 0 0 1 1.414 0L9 10.586V3a1 1 0 1 1 2 0v7.586l1.293-1.293a1 1 0 1 1 1.414 1.414l-3 3a1 1 0 0 1-1.414 0l-3-3a1 1 0 0 1 0-1.414Z" clipRule="evenodd" /></svg>);
@@ -18,6 +16,11 @@ const EyeIcon = () => (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 2
 const EyeSlashIcon = () => (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4"><path fillRule="evenodd" d="M3.28 2.22a.75.75 0 0 0-1.06 1.06l14.5 14.5a.75.75 0 1 0 1.06-1.06l-1.745-1.745a10.029 10.029 0 0 0 3.3-5.59 1.651 1.651 0 0 0 0-1.185A10.004 10.004 0 0 0 9.999 3a9.956 9.956 0 0 0-4.744 1.194L3.28 2.22ZM7.752 6.69l1.092 1.092a2.5 2.5 0 0 1 3.374 3.373l1.091 1.092a4 4 0 0 0-5.557-5.557Z" clipRule="evenodd" /><path d="M10.748 13.93 5.39 8.57a10.015 10.015 0 0 0-3.39 1.42 1.651 1.651 0 0 0 0 1.186A10.004 10.004 0 0 0 9.999 17c1.9 0 3.682-.534 5.194-1.465l-2.637-2.637a3.987 3.987 0 0 1-1.808.032Z" /></svg>);
 const PencilIcon = () => (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4"><path d="m2.695 14.762-1.262 3.155a.5.5 0 0 0 .65.65l3.155-1.262a4 4 0 0 0 1.343-.886L17.5 5.501a2.121 2.121 0 0 0-3-3L3.58 13.419a4 4 0 0 0-.885 1.343Z" /></svg>);
 const CompareIcon = () => (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4"><path d="M10 17a.75.75 0 0 1-.75-.75V5.612L5.29 9.77a.75.75 0 0 1-1.08-1.04l5.25-5.5a.75.75 0 0 1 1.08 0l5.25 5.5a.75.75 0 1 1-1.08 1.04l-3.96-4.158V16.25A.75.75 0 0 1 10 17Z" /></svg>);
+const ListBulletIcon = () => (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4"><path fillRule="evenodd" d="M2 3.75A.75.75 0 0 1 2.75 3h14.5a.75.75 0 0 1 0 1.5H2.75A.75.75 0 0 1 2 3.75Zm0 4.167a.75.75 0 0 1 .75-.75h14.5a.75.75 0 0 1 0 1.5H2.75a.75.75 0 0 1-.75-.75Zm0 4.166a.75.75 0 0 1 .75-.75h14.5a.75.75 0 0 1 0 1.5H2.75a.75.75 0 0 1-.75-.75Zm0 4.167a.75.75 0 0 1 .75-.75h14.5a.75.75 0 0 1 0 1.5H2.75a.75.75 0 0 1-.75-.75Z" clipRule="evenodd" /></svg>);
+const RectangleStackIcon = () => (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4"><path d="M5.127 3.502c.2.019.4.038.598.058l.175.018a47.092 47.092 0 0 0 3.237.24c.718.036 1.439.057 2.163.064l.95.006c.65 0 1.302-.005 1.954-.015.65-.01 1.304-.025 1.957-.045.312-.01.625-.02.937-.033a1.5 1.5 0 0 1 1.55 1.433l.034.338c.026.26.046.52.062.782.03.52.046 1.04.046 1.562 0 .56-.018 1.119-.054 1.677l-.027.424a1.5 1.5 0 0 1-1.536 1.402l-1.356.027a47.457 47.457 0 0 1-3.264.025 47.472 47.472 0 0 1-3.265-.025l-1.356-.027a1.5 1.5 0 0 1-1.536-1.402l-.027-.424a47.382 47.382 0 0 1-.054-1.677c0-.522.016-1.042.046-1.562l.062-.782a1.5 1.5 0 0 1 1.535-1.393ZM2.872 7.72l.061.782a48.887 48.887 0 0 0 .047 1.562c.036.558.054 1.117.054 1.677 0 .522-.016 1.042-.046 1.562l-.062.782a1.5 1.5 0 0 1-1.535 1.393L1.216 15.46a47.094 47.094 0 0 1-3.237-.24 47.462 47.462 0 0 1-3.265-.417l-.175-.027a1.5 1.5 0 0 1-1.324-1.63l.027-.424c.036-.558.054-1.117.054-1.677 0-.522-.016-1.042-.046-1.562l-.062-.782a1.5 1.5 0 0 1 1.324-1.63l.175-.027a47.383 47.383 0 0 1 3.265-.417 47.09 47.09 0 0 1 3.237-.24l.175-.018a1.5 1.5 0 0 1 1.55 1.433Z" /></svg>);
+const ArchiveBoxIcon = () => (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4"><path d="M2 3a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3Z" /><path fillRule="evenodd" d="M13 9a1 1 0 1 0 0 2h-6a1 1 0 1 0 0-2h6ZM2.75 7A.75.75 0 0 0 2 7.75v8.5c0 .69.56 1.25 1.25 1.25h13.5c.69 0 1.25-.56 1.25-1.25v-8.5A.75.75 0 0 0 17.25 7H2.75Z" clipRule="evenodd" /></svg>);
+const CheckIcon = () => (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4 text-green-400"><path fillRule="evenodd" d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z" clipRule="evenodd" /></svg>);
+const XMarkIcon = () => (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4 text-red-400"><path d="M6.28 5.22a.75.75 0 0 0-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 1 0 1.06 1.06L10 11.06l3.72 3.72a.75.75 0 1 0 1.06-1.06L11.06 10l3.72-3.72a.75.75 0 0 0-1.06-1.06L10 8.94 6.28 5.22Z" /></svg>);
 
 interface SidebarProps {
     tracks: Track[];
@@ -90,12 +93,19 @@ const Sidebar: React.FC<SidebarProps> = (props) => {
         onStartRace, onGoToEditor, onDeleteTrack, onDeleteSelected, onViewDetails, 
         hoveredTrackId, onTrackHoverStart, onTrackHoverEnd, simulationState, 
         onOpenDiary, showExplorer, onToggleExplorer,
-        onOpenHub, onOpenPerformanceAnalysis, onOpenSocial, onCompareSelected
+        onOpenHub, onOpenPerformanceAnalysis, onOpenSocial, onCompareSelected,
+        onUpdateTrackMetadata
     } = props;
 
     const fileInputRef = useRef<HTMLInputElement>(null);
     const [searchTerm, setSearchTerm] = useState('');
     const [grouping, setGrouping] = useState<'date' | 'month' | 'folder' | 'type'>('month');
+    const [viewMode, setViewMode] = useState<'cards' | 'list'>('cards');
+    const [showArchived, setShowArchived] = useState(false);
+    
+    // Inline Renaming State
+    const [editingId, setEditingId] = useState<string | null>(null);
+    const [editName, setEditName] = useState('');
 
     const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         if (e.target.files && e.target.files.length > 0) {
@@ -106,8 +116,12 @@ const Sidebar: React.FC<SidebarProps> = (props) => {
     const isSimulationInProgress = simulationState === 'running' || simulationState === 'paused' || simulationState === 'finished';
 
     const filteredTracks = useMemo(() => {
-        return tracks.filter(t => t.name.toLowerCase().includes(searchTerm.toLowerCase()));
-    }, [tracks, searchTerm]);
+        return tracks.filter(t => {
+            const matchesSearch = t.name.toLowerCase().includes(searchTerm.toLowerCase());
+            const matchesArchive = showArchived ? true : !t.isArchived;
+            return matchesSearch && matchesArchive;
+        });
+    }, [tracks, searchTerm, showArchived]);
 
     const groupedTracks = useMemo(() => {
         const groups: Record<string, Track[]> = {};
@@ -127,6 +141,29 @@ const Sidebar: React.FC<SidebarProps> = (props) => {
         });
         return groups;
     }, [filteredTracks, grouping]);
+
+    // Renaming handlers
+    const startRenaming = (track: Track) => {
+        setEditingId(track.id);
+        setEditName(track.name);
+    };
+
+    const saveRename = () => {
+        if (editingId && editName.trim()) {
+            onUpdateTrackMetadata(editingId, { name: editName.trim() });
+        }
+        setEditingId(null);
+    };
+
+    const cancelRename = () => {
+        setEditingId(null);
+        setEditName('');
+    };
+
+    const handleRenameKeyDown = (e: React.KeyboardEvent) => {
+        if (e.key === 'Enter') saveRename();
+        if (e.key === 'Escape') cancelRename();
+    };
 
     return (
         <div className="flex flex-col h-full bg-slate-900 border-r border-slate-800 text-white">
@@ -155,17 +192,35 @@ const Sidebar: React.FC<SidebarProps> = (props) => {
                     className="w-full bg-slate-800 border border-slate-700 rounded px-3 py-1.5 text-sm text-white focus:border-cyan-500 outline-none"
                 />
                 
-                <div className="flex justify-between items-center text-xs">
+                <div className="flex items-center gap-2 text-xs">
                     <select 
                         value={grouping} 
                         onChange={(e) => setGrouping(e.target.value as any)}
-                        className="bg-slate-800 border border-slate-700 rounded px-2 py-1 text-slate-300 outline-none"
+                        className="bg-slate-800 border border-slate-700 rounded px-2 py-1 text-slate-300 outline-none flex-grow"
                     >
                         <option value="month">Mese</option>
                         <option value="type">Tipo</option>
                         <option value="folder">Cartella</option>
                     </select>
                     
+                    <button 
+                        onClick={() => setViewMode(viewMode === 'cards' ? 'list' : 'cards')}
+                        className={`p-1.5 rounded border ${viewMode === 'list' ? 'bg-cyan-600 text-white border-cyan-500' : 'bg-slate-800 text-slate-400 border-slate-700 hover:text-white'}`}
+                        title="Cambia Vista (Elenco/Schede)"
+                    >
+                        {viewMode === 'cards' ? <RectangleStackIcon /> : <ListBulletIcon />}
+                    </button>
+
+                    <button 
+                        onClick={() => setShowArchived(!showArchived)}
+                        className={`p-1.5 rounded border ${showArchived ? 'bg-amber-600 text-white border-amber-500' : 'bg-slate-800 text-slate-400 border-slate-700 hover:text-white'}`}
+                        title={showArchived ? "Nascondi Archivio" : "Mostra Archivio"}
+                    >
+                        <ArchiveBoxIcon />
+                    </button>
+                </div>
+
+                <div className="flex justify-between items-center text-[10px]">
                     <div className="flex gap-2">
                         <button onClick={onSelectAll} className="text-cyan-400 hover:text-cyan-300">Tutti</button>
                         <button onClick={onDeselectAll} className="text-slate-400 hover:text-slate-200">Nessuno</button>
@@ -192,11 +247,11 @@ const Sidebar: React.FC<SidebarProps> = (props) => {
                 {Object.entries(groupedTracks).map(([group, groupTracks]) => (
                     <div key={group}>
                         <h3 className="text-xs font-bold text-slate-500 uppercase tracking-widest mb-2 px-2 sticky top-0 bg-slate-900 z-10 py-1">{group}</h3>
-                        <div className="space-y-1">
+                        <div className={`space-y-1 ${viewMode === 'list' ? 'space-y-0' : ''}`}>
                             {groupTracks.map(track => (
                                 <div 
                                     key={track.id} 
-                                    className={`flex items-center p-2 rounded hover:bg-slate-800 transition-colors group relative ${hoveredTrackId === track.id ? 'bg-slate-800' : ''}`}
+                                    className={`flex items-center p-2 rounded hover:bg-slate-800 transition-colors group relative ${hoveredTrackId === track.id ? 'bg-slate-800' : ''} ${viewMode === 'list' ? 'border-b border-slate-800/50 py-1' : 'mb-1'}`}
                                     onMouseEnter={() => onTrackHoverStart(track.id)}
                                     onMouseLeave={onTrackHoverEnd}
                                 >
@@ -209,18 +264,40 @@ const Sidebar: React.FC<SidebarProps> = (props) => {
                                         />
                                     </div>
                                     
-                                    <div 
-                                        className="flex-grow min-w-0 cursor-pointer"
-                                        onClick={() => onViewDetails(track.id)}
-                                    >
-                                        <div className="flex justify-between items-center mb-0.5">
-                                            <span className="text-sm font-medium text-white truncate" title={track.name}>{track.name}</span>
-                                            {track.rating && <RatingStars rating={track.rating} size="xs" />}
-                                        </div>
-                                        <div className="flex items-center justify-between text-[10px] text-slate-400">
-                                            <span>{new Date(track.points[0].time).toLocaleDateString()}</span>
-                                            <span className="font-mono">{track.distance.toFixed(2)}km</span>
-                                        </div>
+                                    <div className="flex-grow min-w-0">
+                                        {editingId === track.id ? (
+                                            <div className="flex items-center gap-1">
+                                                <input 
+                                                    type="text" 
+                                                    value={editName}
+                                                    onChange={(e) => setEditName(e.target.value)}
+                                                    onKeyDown={handleRenameKeyDown}
+                                                    className="w-full bg-slate-950 text-white text-xs border border-cyan-500 rounded px-1 py-0.5 outline-none"
+                                                    autoFocus
+                                                    onBlur={saveRename}
+                                                />
+                                                <button onClick={saveRename} className="text-green-400 hover:text-green-300"><CheckIcon /></button>
+                                                <button onClick={cancelRename} className="text-red-400 hover:text-red-300"><XMarkIcon /></button>
+                                            </div>
+                                        ) : (
+                                            <div 
+                                                className="cursor-pointer"
+                                                onClick={() => onViewDetails(track.id)}
+                                                onDoubleClick={(e) => { e.stopPropagation(); startRenaming(track); }}
+                                                title="Doppio clic per rinominare"
+                                            >
+                                                <div className="flex justify-between items-center mb-0.5">
+                                                    <span className={`text-sm font-medium text-white truncate ${viewMode === 'list' ? 'text-xs' : ''}`}>
+                                                        {track.name}
+                                                    </span>
+                                                    {viewMode === 'cards' && track.rating && <RatingStars rating={track.rating} size="xs" />}
+                                                </div>
+                                                <div className="flex items-center justify-between text-[10px] text-slate-400">
+                                                    <span>{new Date(track.points[0].time).toLocaleDateString()}</span>
+                                                    <span className="font-mono">{track.distance.toFixed(2)}km</span>
+                                                </div>
+                                            </div>
+                                        )}
                                     </div>
 
                                     <div className="flex items-center gap-1 ml-2 opacity-0 group-hover:opacity-100 transition-opacity">
@@ -234,7 +311,6 @@ const Sidebar: React.FC<SidebarProps> = (props) => {
                                         <button 
                                             onClick={() => {
                                                 onToggleRaceSelection(track.id);
-                                                // Trigger editor selection via external effect or prop
                                                 onGoToEditor(); 
                                             }}
                                             className="p-1 text-slate-500 hover:text-white"
